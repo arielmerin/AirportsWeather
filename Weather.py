893754141d686097ad6caaffc261a06cf9013a29
arielmerin <arielmerino@ciencias.unam.mx>
2020-10-01 11:44:17 -0500

    Adding this first modules

---
+++ Weather.py
@@ -0,0 +1,19 @@
+import csv
+import requests
+import json
+
+class Weather:
+
+    def __init__(self):
+        self.address = 'http://api.openweathermap.org/data/2.5/weather?'
+        self.apiID = '&APPID=4bc2be4c4fc75d1df5568e38fd570019'
+        self.idi = '&lang=es'
+        self.lat = '&lat='
+        self.lon = '&lon='
+
+
+    def makeApiRequest(self, lat, lon):
+        return requests.get(self.address + self.lat + lat + self.lon + lon + self.apiID + self.idi).json()
+
+
+
---
+++ main.py
@@ -0,0 +1,11 @@
+import json
+import requests
+from Weather import Weather
+
+
+climita = Weather()
+
+
+# res = requests.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=4bc2be4c4fc75d1df5568e38fd570019').json()
+
+print(climita.makeApiRequest('19.3371', '-99.566'))
