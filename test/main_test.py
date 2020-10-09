a4352985bc83c94ac5ba66771f239c282762d263
arielmerin <arielmerino@ciencias.unam.mx>
2020-10-09 00:26:40 -0500

    deleting bulls** from these test

--- test/main_test.py
+++
@@ -1,6 +0,0 @@
-import pytest
-import src.main
-
-class MyTestCase():
-    def test_validate_file(argument):
-        assert
---
+++ test/src/main_test.py
@@ -0,0 +1,9 @@
+import pytest
+
+from main import validate_file
+
+
+class MyTestCase():
+    def test_validate_file(argument):
+        validate_file("hi")
+        assert True
