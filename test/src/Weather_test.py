ea69304aa4cc605c54f7ef2d119e7905780e37a9
ArmandoAAC11 <armandoaac@ciencias.unam.mx>
2020-10-09 19:27:55 -0500

    Pura tonteria en el test Weather

--- test/src/Weather_test.py
+++ test/src/Weather_test.py
@@ -1,8 +1,25 @@
+import json
+import unittest
+import urllib3
 import pytest
 
 
-class Weather_test:
-    def test_make_api_request_by_coordinates(self, lat, lon):
-        assert (True, False)
+class Weather_test(unittest.TestCase):
+    def test_make_api_request_by_coordinates(self,lat,lon):
+        testurl = 'http://api.openweathermap.org/data/2.5/weather?'
+        #print(testurl)
+        response = urllib3.urlopen(testurl)
+        ans = response.read()
+        self.assertTrue()
+
+    def tearDown(self):
+        print("-------TEST IS OVER--------")
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+    """def test_make_api_request_by_coordinates(self, lat, lon):
+        assert (True, False)"""
 
 
